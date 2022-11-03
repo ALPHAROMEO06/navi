a118642e769b3299bd15fd987cce478abe6071b9
ALPHAROMEO06 <111852979+ALPHAROMEO06@users.noreply.github.com>
2022-11-03 14:38:13 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,19 +1,19 @@
-BOT_TOKEN = "467677575:YZfaakjwd545dfg-N6JStihhuw5gQeZHntc"
-API_ID = 123456
+BOT_TOKEN = "5482708987:AAGcmMexDVp9n1C-Qm6dJXpLI4Dy0Z524Ko"
+API_ID = 14782914
 SESSION_STRING = "" # Check Readme to generate sessions; need for userbot
-API_HASH = "dfxcgs5s12hdcxfgdfz"
+API_HASH = "3aa2fabe1074632cf6e2b01da083a2c6"
 USERBOT_PREFIX = "."
-PHONE_NUMBER = "+916969696969"  # Need for Userbot
+PHONE_NUMBER = "+918220640614"  # Need for Userbot
 SUDO_USERS_ID = [
-    4543744343,
-    543214651351,
+    5544792640,
+    1930954213,
 ]  # Sudo users have full access to everything, don't trust anyone
-LOG_GROUP_ID = -100125431255
-GBAN_LOG_GROUP_ID = -100125431255
-MESSAGE_DUMP_CHAT = -1001181696437
+LOG_GROUP_ID = -832346390
+GBAN_LOG_GROUP_ID = -832346390
+MESSAGE_DUMP_CHAT = -832346390
 WELCOME_DELAY_KICK_SEC = 300
-MONGO_URL = "mongodb+srv://username:password@cluster0.ksiis.mongodb.net/YourDataBaseName?retryWrites=true&w=majority"
-ARQ_API_KEY = "Get this from @ARQRobot"
+MONGO_URL = "mongodb+srv://shikhar:shikhar@cluster0.6xzlh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+ARQ_API_KEY = "JGAGCN-IRAJKY-PTPEXD-FCDGHX-ARQ"
 ARQ_API_URL = "https://arq.hamker.in"
 LOG_MENTIONS = True
 RSS_DELAY = 300  # In seconds
