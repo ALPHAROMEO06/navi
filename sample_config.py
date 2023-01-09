25bf3156053cf5fa701cdb791daf65bc40e522b4
ALPHAROMEO06 <111852979+ALPHAROMEO06@users.noreply.github.com>
2023-01-09 11:13:39 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,16 +1,16 @@
-BOT_TOKEN = "5482708987:AAGcmMexDVp9n1C-Qm6dJXpLI4Dy0Z524Ko"
+BOT_TOKEN = "5165959972:AAEXWdylwty0pVDz6X_8hZw1NL8EqTfNByk"
 API_ID = 14782914
-SESSION_STRING = "BQB9haT9u78_ZRG_pqTe0VQSpPnjF_MJ9dg_D9G3LOi2_v3X5txqCcItCzUa7v1lxAv7neM4HywxZYf3WQ6OAxyiJRPzxObloklAlAevMe8nhPVVuXb3dIyomo5G6nZV1gthFEcZ_nwxokJT_FBtLxsyBoLYIvJgTuEInivsJcLTgdE5Pgw5J703QJqTACL83lEjnoX0Upuez3hHnFMHvb1Mre16-MyUcble3Hd9_p9YxN06IgasJx5CkrBKID1PyHLEV8j9PH6Bz5vaiz7R6bpWKoqb9GeC8097gK7M8yx_tcTG17ho_1cOZH0-SdCZ7zVPulaF36ig3Gps-Kev6iHAAAAAAUp-0kAA" # Check Readme to generate sessions; need for userbot
+SESSION_STRING = "" # Check Readme to generate sessions; need for userbot
 API_HASH = "3aa2fabe1074632cf6e2b01da083a2c6"
 USERBOT_PREFIX = "."
-PHONE_NUMBER = "+918220640614"  # Need for Userbot
+PHONE_NUMBER = "+916381987933"  # Need for Userbot
 SUDO_USERS_ID = [
-    5544792640,
+    5938516793,
     1930954213,
 ]  # Sudo users have full access to everything, don't trust anyone
-LOG_GROUP_ID = -832346390
-GBAN_LOG_GROUP_ID = -832346390
-MESSAGE_DUMP_CHAT = -832346390
+LOG_GROUP_ID = -1001657538863
+GBAN_LOG_GROUP_ID = -1001657538863
+MESSAGE_DUMP_CHAT = -1001657538863
 WELCOME_DELAY_KICK_SEC = 300
 MONGO_URL = "mongodb+srv://shikhar:shikhar@cluster0.6xzlh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 ARQ_API_KEY = "JGAGCN-IRAJKY-PTPEXD-FCDGHX-ARQ"
